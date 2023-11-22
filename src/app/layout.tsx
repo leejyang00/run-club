32d829feb97d7b9bb7b86bf0a83202a48fad4c52
Jian Yang Lee <leejyang00@gmail.com>
2023-11-23 02:07:05 +1000

    temp fix for layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import InfoBar from "./common/InfoBar";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+// import InfoBar from "./common/InfoBar";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,10 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className} suppressHydrationWarning={true}>
-        <InfoBar />
-        <Header />
+        {/* <InfoBar />
+        <Header /> */}
         {children}
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Centre from "./components/Centre";
+import InfoBar from "./common/InfoBar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col">
+      <InfoBar />
+      <Header />
       <Centre />
+      <Footer />
     </main>
   );
 }
