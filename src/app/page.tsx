48bd8cfd84d7e29d3c336fb0cb9a05ade732fc40
Jian Yang Lee <leejyang00@gmail.com>
2023-12-16 15:59:15 +1000

    PCE-16: created story and blog page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
-import Centre from "./components/centre";
-import InfoBar from "./common/InfoBar";
-import Header from "./components/header";
-import Footer from "./components/footer";
+// import InfoBar from "./common/InfoBar";
+// import Header from "./components/header";
+// import Footer from "./components/footer";
+
+import Centre from "./components/Centre";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col">
-      <InfoBar />
-      <Header />
-      <Centre />
-      <Footer />
-    </main>
+    <Centre />
+    // <main className="min-h-screen flex flex-col">
+      // {/* <InfoBar />
+      // <Header /> */}
+      // {/* <Footer />   */}
+    // </main>
   );
 }
---
+++ src/pages/blog/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Header from '../../app/components/Header';
+import "../globals.css";
+
+export default function Bloggg() {
+  return (
+    <div>
+      <Header />
+      <div>Bloggg</div>
+    </div>
+  );
+}
